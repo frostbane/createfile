85b19dd148c32bd473b407fb09d47d721ce6a78f
frostbane <frostbane@programmer.net>
2023-12-03 17:40:03 +1000

    init

---
+++ src/Fb/Arguments.hs
@@ -0,0 +1,43 @@
+{-# OPTIONS -Wall -Wno-unused-matches #-}
+
+
+module Fb.Arguments
+    ( getWorkingDir,
+      getProgram,
+      getArguments,
+    )
+  where
+
+import System.Environment
+    ( getProgName,
+      getArgs,
+    )
+import System.Directory (getCurrentDirectory)
+import System.IO
+    ( hSetEncoding,
+      stdin,
+      stdout,
+      utf8,
+    )
+import qualified Data.Text as T
+import Data.Text
+import Data.Text.IO (hPutStrLn)
+
+
+getWorkingDir :: IO (Text)
+getWorkingDir = do
+    cwd <- getCurrentDirectory
+    return $ T.pack cwd
+
+getProgram :: IO (Text)
+getProgram = do
+    name <- getProgName
+    return $ T.pack name
+
+getArguments :: IO ([Text])
+getArguments = do
+    hSetEncoding stdin utf8
+    hSetEncoding stdout utf8
+    args <- getArgs
+    return $ Prelude.map T.pack args
+
