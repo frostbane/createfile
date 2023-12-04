9fafc1d879ea04468d87e9eef76bec3650e14a45
frostbane <frostbane@programmer.net>
2023-12-04 14:57:32 +1000

    test argument count

--- test/Spec/Validation.hs
+++ test/Spec/Validation.hs
@@ -1,15 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Spec.Validation
     ( validationSpec
     ) where
 
 import Test.Hspec
+import System.IO
+import System.Directory
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Identity
+
+import Lib
 
 validationSpec :: Spec
-validationSpec =
+validationSpec = do
+  it "info" $ do
+    pwd <- (return . show) =<< getCurrentDirectory
+    putStrLn $ "\t" ++ pwd
   describe "program argument validation" $ do
     context "argument count" $ do
       it "should have 2 or more arguments" $ do
-        pendingWith "not yet implemeted"
+        argumentCountIsEnough ["a"] `shouldBe` False
+        argumentCountIsEnough ["a", "b"] `shouldBe` True
+        argumentCountIsEnough ["a", "b", "c"] `shouldBe` True
     context "target output file" $ do
       it "output folder should exist" $ do
         pendingWith "not yet implemeted"
